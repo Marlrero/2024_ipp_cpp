270e6c16b3e2d5fe56d9dc418e7ec0532ed59221
marlrero <mrtj0329@gmail.com>
2024-06-18 12:24:01 +0900

    feat first commit

---
+++ first copy.c
@@ -0,0 +1,15 @@
+/*
+    C Hello World Program
+*/
+
+// C Math, I/O Library
+#include <stdio.h>
+#include <math.h>
+
+// 메인함수 정의
+int main() {
+    printf("Hello World\n"); // 구문은 세미콜론으로 끝남
+    printf("sqrt(4) is %f\n", sqrt(4));
+
+    return 0; // main은 0을 반환
+}
